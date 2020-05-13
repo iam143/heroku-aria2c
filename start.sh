32b37c02b0ac8f78dac4bf008d317dd98c03136e
iam143 <61903214+iam143@users.noreply.github.com>
2020-05-13 15:16:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,7 @@
 
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
-	echo "[DRIVE]" > rclone.conf
-	echo "$(echo $RCLONE_CONFIG_BASE64|base64 -d)" >> rclone.conf
+	echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
 	echo "on-download-stop=./delete.sh" >> aria2c.conf
 	echo "on-download-complete=./on-complete.sh" >> aria2c.conf
 	chmod +x delete.sh
